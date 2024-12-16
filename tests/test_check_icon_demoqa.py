cf004d9212701d4b0ba00ae9ddd12c40152f4290
ViceSergo <sergeitrufanov73@mail.ru>
2024-12-16 22:07:32 +0300

    First commit

--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -4,4 +4,4 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get('https://demoqa.com/')
 
-icon = driver.find_element(By.CSS_SELECTOR, '')
+icon = driver.find_element(By.CSS_SELECTOR, '"')
