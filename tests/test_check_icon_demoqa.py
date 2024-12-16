8014181b0352cab84bd9600d5ade7e3f2b9acccf
ViceSergo <sergeitrufanov73@mail.ru>
2024-12-16 22:18:37 +0300

    check icon

--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get('https://demoqa.com/')
 
-icon = driver.find_element(By.CSS_SELECTOR, '"')
+def test_check_icon():
+    driver = webdriver.Chrome()
+    driver.get('https://demoqa.com/')
+
+    icon = driver.find_element(By.CSS_SELECTOR, '#app>header>a')
+
+    if icon is None:
+        print("Не найдено")
+    else:
+        print("Найдено")
