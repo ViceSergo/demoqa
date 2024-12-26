5a6276751bf1f4b99f2187bb92ccf8489b721634
ViceSergo <sergeitrufanov73@mail.ru>
2024-12-26 20:22:24 +0300

    check icon

--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -3,18 +3,9 @@ from pages.demoqa import DemoQa
 import time
 
 
-def test_check_icon(browser):
-    # browser.get('https://demoqa.com/')
-    #
-    # icon = browser.find_element(By.CSS_SELECTOR, '#app>header>a')
-    #
-    # if icon is None:
-    #     print("Не найдено")
-    # else:
-    #     print("Найдено")
-
+def test_icon_exist(browser):
     demo_qa_page =DemoQa(browser)
     demo_qa_page.visit()
-    demo_qa_page.click_on_the_icon()
+    demo_qa_page.icon.click()
     assert demo_qa_page.equal_url()
-    assert demo_qa_page.exist_icon()
+    assert demo_qa_page.icon.exist()
---
+++ tests/test_go_to_page_elements.py
@@ -0,0 +1,14 @@
+# from selenium.webdriver.common.by import By
+from pages.demoqa import DemoQa
+from pages.elements_page import ElementsPage
+import time
+
+
+def test_go_to_page_elements(browser):
+    demo_qa_page = DemoQa(browser)
+    elements_page = ElementsPage(browser)
+
+    demo_qa_page.visit()
+    assert demo_qa_page.equal_url()
+    demo_qa_page.btn_elements.click()
+    assert elements_page.equal_url()
